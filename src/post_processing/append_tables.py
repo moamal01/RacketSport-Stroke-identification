b0dce84a9b47bdc3e8eeec3a64a8c21b1e7a9cf1
Moamal <moamal001@gmail.com>
2025-05-29 20:57:42 +0200

    Remove Sequence frame

--- src/post_processing/append_tables.py
+++ src/post_processing/append_tables.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-video = 3
+video = 2
 bbox = False
 # Read the two CSV files
 if bbox:
-    df1 = pd.read_csv(f'../../data/video_{video}/bbox_video{video}_incomp.csv')
-    df2 = pd.read_csv(f'../../data/video_{video}/bbox_video{video}_no_strokes.csv')
+    df1 = pd.read_csv(f'../../data/video_{video}/bbox_video{video}q3.csv')
+    df2 = pd.read_csv(f'../../data/video_{video}/bbox_video{video}full4.csv')
 else:
-    df1 = pd.read_csv(f'../../data/video_{video}/keypoints_video{video}_incomp.csv')
-    df2 = pd.read_csv(f'../../data/video_{video}/keypoints_video{video}_no_strokes.csv')
+    df1 = pd.read_csv(f'../../data/video_{video}/keypoints_video{video}q3.csv')
+    df2 = pd.read_csv(f'../../data/video_{video}/keypoints_video{video}full4.csv')
 
 # Combine the two dataframes
 combined_df = pd.concat([df1, df2])
 
 # Sort by the 'frame' column
-sorted_df = combined_df.sort_values(by=['Event frame', 'Sequence frame'])
+sorted_df = combined_df.sort_values(by=['Event frame'])
 
 # Optionally reset the index
 sorted_df.reset_index(drop=True, inplace=True)
