aee0e7e8604e68620bc809cf499818b41c132c5e
Moamal <moamal001@gmail.com>
2025-06-04 20:02:02 +0200

    Plot prediction probabilities

--- src/plotting/probabilities.py
+++ src/plotting/probabilities.py
@@ -11,22 +11,38 @@ import seaborn as sns
 plt.rcParams.update({'font.size': 22})
 sns.set_theme()
 
-# OBS: Needs to run through testvid again
-
-model = joblib.load('../../results/default/20250522_002633/14_norm_keypoints_midpoints_table_time_ball/logistic_regression_model.joblib')
-label_encoder = joblib.load('../../results/default/20250522_002633/14_norm_keypoints_midpoints_table_time_ball/label_encoder.joblib')
+model = joblib.load('../../results/default/20250603_145008/14_norm_keypoints_midpoints_table_time_ball/logistic_regression_model.joblib')
+label_encoder = joblib.load('../../results/default/20250603_145008/14_norm_keypoints_midpoints_table_time_ball/label_encoder.joblib')
 
 start_frame = 5
-end_frame = 1020
+end_frame = 541
 frames_kept = []
 
-left_serve = 926
+right_serve = 424
+left_backhand = 512
 
 probabilities = []
 
 for i in tqdm(range(start_frame, end_frame), desc="Processing Frames"):
-    x = get_feature(4, [i], 5, 1, False, True, True, False, True,
-                    True, False, False, "fall_back", False, True, True)
+    x = get_feature(
+                video_number=4,
+                frames=[i],
+                sequence_range=90,
+                sequence_gap=2,
+                raw=False,
+                add_keypoints=True,
+                add_midpoints=True,
+                add_rackets=False,
+                add_table=True,
+                add_ball=True,
+                add_scores=False,
+                add_embeddings=False,
+                missing_strat="fall_back",
+                mirror=False,
+                simplify=True,
+                long_edition=True
+            )
+    
     if x is None:
         continue
 
@@ -43,12 +59,16 @@ for cls_idx, cls_name in enumerate(label_encoder.classes_):
     plt.plot(frames_kept, probabilities[:, cls_idx], label=cls_name)
 
 # Add serve timestamp
-plt.axvline(x=left_serve, color='red', linestyle='--', linewidth=2, label='Left Serve')
-plt.text(left_serve + 5, 0.9, 'Left Serve', color='red')
+plt.axvline(x=right_serve, color='red', linestyle='--', linewidth=1, label='Right Serve')
+plt.text(right_serve + 5, 0.9, 'Right Serve', color='red')
+plt.axvline(x=left_backhand, color='red', linestyle='--', linewidth=1, label='Left Backhand')
+plt.text(left_backhand + 5, 0.9, 'Left backhand', color='red')
 
 plt.xlabel("Frame index")
 plt.ylabel("Predicted probability")
 plt.title("Per-frame stroke probabilities")
-plt.legend()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
 plt.tight_layout()
+plt.savefig("stroke_probabilities_plot_nophases.png", dpi=300, bbox_inches='tight') 
 plt.show()
